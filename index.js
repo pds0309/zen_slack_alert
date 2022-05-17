81fb92e4608ed72b09e381cb896637fed0d460b5
pds0309 <ehd0309@gmail.com>
2022-05-18 08:49:21 +0900

    fix: 수정1

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 // 필수입니다.
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 const ALERT_PIPELINES = process.env.ALERT_PIPELINES;
-
 // 옵션입니다.
 const ALERT_PIPELINES_ALIAS = process.env.ALERT_PIPELINES_ALIAS
   ? process.env.ALERT_PIPELINES_ALIAS
@@ -12,13 +11,18 @@ const ALERT_PIPELINES_ALIAS = process.env.ALERT_PIPELINES_ALIAS
 const REPO = process.env.REPO;
 const REPO_ALIAS = process.env.REPO_ALIAS ? process.env.REPO_ALIAS : "";
 
-const baseURL = "https://app.zenhub.com/workspace/o";
+const baseUrl = "https://app.zenhub.com/workspace/o";
 const permitEvent = "issue_transfer";
 const stringToArray = (str) => (str || "").split(",").map((t) => t.trim());
 
 const alertPipelineArray = stringToArray(ALERT_PIPELINES);
+const repoArray = stringToArray(REPO);
 const pipelinesMap = alertPipelineArray.reduce(
   (o, k, i) => ({ ...o, [k]: stringToArray(ALERT_PIPELINES_ALIAS)[i] }),
+  {}
+);
+const repoMap = repoArray.reduce(
+  (o, k, i) => ({ ...o, [k]: stringToArray(REPO_ALIAS)[i] }),
   {}
 );
 
@@ -39,31 +43,44 @@ const pipelinesMap = alertPipelineArray.reduce(
 }
 */
 
-const getIssueInfo = (query) => {
-  `<${baseURL}/${query.organization}/${query.repo}/issues/${query.issue_number}|#${query.issue_number} ${data.issue_title}>`;
+const getIssueInfo = (data) => {
+  return `<${baseUrl}/${data.organization}/${data.repo}/issues/${data.issue_number}|#${data.issue_number} ${data.issue_title}>`;
 };
 
-const issueMessages = (query) =>
-  `**${query.user_name}** :  [${getIssueInfo(query)}] ISSUE의 상태를 ${
-    query.to_pipeline_name
-  } ${pipelinesMap[query.to_pipeline_name] || ""} 로 변경했어요!`.trim();
+const issueMessages = (data) =>
+  `${repoMap[data.repo]}: <${data.user_name}> :  ${getIssueInfo(
+    data
+  )} ISSUE의 상태를  ${
+    pipelinesMap[data.to_pipeline_name] || ""
+  } 로 변경했어요!`.trim();
 
 const base64decode = (base64text) => {
   return Buffer.from(base64text, "base64").toString("utf8");
 };
 
-exports.handler = async (event, callback) => {
+const getURLSearchParamsToJSON = (string) => {
+  const searchParams = new URLSearchParams(string);
+  let object = {};
+  searchParams.forEach((value, key) => {
+    const keys = key.split("."),
+      last = keys.pop();
+    keys.reduce((r, a) => (r[a] = r[a] || {}), object)[last] = value;
+  });
+  return object;
+};
+
+exports.handler = (event, _, callback) => {
   if (!event.body) {
     callback("이벤트 경로 정보를 찾을 수 없습니다.");
   }
-  const data = new URLSearchParams(base64decode(event.body));
-  const message = issueMessages(data);
+  const data = getURLSearchParamsToJSON(base64decode(event.body));
+  const text = issueMessages(data);
+
   if (data.type && data.type === permitEvent) {
     if (alertPipelineArray.includes(data.to_pipeline_name)) {
-      axios.post(WEBHOOK_URL, { message, link_names: 1 }).catch(console.log);
+      axios.post(WEBHOOK_URL, { text, link_names: 1 }).catch(console.log);
     }
   }
-
   callback(null, {
     statusCode: "200",
     headers: { "Content-Type": "application/json" },
